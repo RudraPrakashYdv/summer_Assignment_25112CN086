f64fa9f56c2b51a464916bfa1a43e94d23300a40
RudraPrakashYdv <mantuyadavyadav16@gmail.com>
2026-06-03 13:25:09 +0530

    Add files via upload

---
+++ Day_4/armstrong.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, original, rem;
+    int sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while(num > 0) {
+        rem = num % 10;
+        sum += rem * rem * rem;
+        num /= 10;
+    }
+
+    if(sum == original)
+        printf("%d is an Armstrong number", original);
+    else
+        printf("%d is not an Armstrong number", original);
+
+    return 0;
+}
