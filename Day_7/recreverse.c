b02d8d191f05a91584e975a71c338af7d8bfc1a7
RudraPrakashYdv <mantuyadavyadav16@gmail.com>
2026-06-05 11:55:24 +0530

    Add files via upload

---
+++ Day_7/recreverse.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int rev = 0;
+
+int reverseNumber(int n)
+{
+    if(n == 0)
+        return rev;
+
+    rev = rev * 10 + n % 10;
+    return reverseNumber(n / 10);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Reverse Number = %d", reverseNumber(n));
+
+    return 0;
+}
